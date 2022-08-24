e97cbd99930cd83f9d548d52b817c3a377f6f673
Viliczech <janvilimek.info@gmail.com>
2022-08-25 01:44:00 +0200

    Bing bong

---
+++ src/components/projects/list/ProjectListItem.tsx
@@ -0,0 +1,36 @@
+import { IonButton, IonCard,IonCardContent,IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonImg, IonItem, IonLabel, IonTitle } from "@ionic/react";
+import { eyeOffOutline } from "ionicons/icons";
+
+interface ProjectListItemProps {
+    name: string;
+    date: string;
+    image: string;
+    shared: boolean;
+}  
+export const ProjectListItem: React.FC<ProjectListItemProps> = ({ name, date, image }) => {
+    const routeToProjectDetail = (data: any) => {
+        console.error("parek", data)
+    }
+
+    return (
+        <IonCard onClick={routeToProjectDetail} href="/project" routerLink="" routerDirection="forward" style={
+            {
+                backgroundImage: `linear-gradient(to left, rgba(245, 246, 252, 0.52), rgb(0 0 0 / 73%)), url('${image}')`, 
+                backgroundPosition: 'center',
+                backgroundSize: 'cover'
+            }
+        }>
+            <IonCardHeader >
+                <IonCardSubtitle>{date}</IonCardSubtitle>
+                <IonCardTitle>{name}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                <span>
+                    <IonIcon slot="start" icon={eyeOffOutline}></IonIcon>
+                </span>
+                
+                
+            </IonCardContent>
+        </IonCard>
+    );
+}
